4e7df116135fffe3c2dd5bcb5e9a5724160f52b6
Keagan Rodrigues <keagan123@outlook.com>
2026-04-04 06:45:49 -0400

    Initial Commit

---
+++ QuotesAppWidget/QuotesAppWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuotesAppWidgetApp.swift
+//  QuotesAppWidget
+//
+//  Created by Keagan Rodrigues on 2026-04-04.
+//
+
+import SwiftUI
+
+@main
+struct QuotesAppWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
